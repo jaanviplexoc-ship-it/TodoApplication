8518b3f22683c729ad182232619e333adcb71e37
Janvi kuchhadiya <Jaanvi.plexoc@gmail.com>
2026-09-03 14:34:39 +0530

    test commit

--- server/TodoApp.Api/obj/Release/net8.0/TodoApp.Api.AssemblyInfo.cs
+++ server/TodoApp.Api/obj/Release/net8.0/TodoApp.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+820bd6573d82cac0d34ef5a2e82af00479b5ac0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
